bf7deccac11d6f46b77bf304ffd14f0305167da1
simon-gridex <developer@gridex.org>
2023-04-11 14:54:26 +0800

    add holders

--- subgraphs/holder/src/gdx.ts
+++ subgraphs/holder/src/gdx.ts
@@ -1,37 +1,44 @@
 import {Transfer as TransferEvent} from "../generated/GDX/GDX";
-import {Holder, TotalSupply} from "../generated/schema";
+import {Holder, Overview} from "../generated/schema";
 import {Address, BigDecimal, BigInt, store} from "@graphprotocol/graph-ts";
+import {BIG_DECIMAL_ZERO, BIG_INT_ONE, BIG_INT_ZERO} from "../../core/src/base/helper/consts";
 
 export function handleTransfer(event: TransferEvent): void {
     const value = event.params.value;
-    if (value == BigInt.zero()) {
+    if (value == BIG_INT_ZERO) {
         return;
     }
     const from = event.params.from;
     const to = event.params.to;
     const amount = value.toBigDecimal().div(BigDecimal.fromString("1e18"));
 
-    handleTotalSupply(from, to, amount);
+    handleOverview(from, to, amount);
     handleHolder(from, amount.neg());
     handleHolder(to, amount);
 }
 
-function handleTotalSupply(from: Address, to: Address, amount: BigDecimal): void {
+function handleOverview(from: Address, to: Address, amount: BigDecimal): void {
     if (from != Address.zero() && to != Address.zero()) {
         return;
     }
-    let totalSupply = TotalSupply.load("GDX");
-    if (totalSupply == null) {
-        totalSupply = new TotalSupply("GDX");
-        totalSupply.amount = BigDecimal.zero();
-    }
+    let overview = loadOrNewOverview();
     if (from == Address.zero()) {
-        totalSupply.amount = totalSupply.amount.plus(amount);
+        overview.totalSupply = overview.totalSupply.plus(amount);
     }
     if (to == Address.zero()) {
-        totalSupply.amount = totalSupply.amount.minus(amount);
+        overview.totalSupply = overview.totalSupply.minus(amount);
     }
-    totalSupply.save();
+    overview.save();
+}
+
+function loadOrNewOverview(): Overview {
+    let overview = Overview.load("GDX");
+    if (overview == null) {
+        overview = new Overview("GDX");
+        overview.totalSupply = BIG_DECIMAL_ZERO;
+        overview.holders = BIG_INT_ZERO;
+    }
+    return overview;
 }
 
 function handleHolder(address: Address, amount: BigDecimal): void {
@@ -39,14 +46,23 @@ function handleHolder(address: Address, amount: BigDecimal): void {
         return;
     }
     let holder = Holder.load(address);
+    let delta = BIG_INT_ZERO;
     if (holder == null) {
+        delta = BIG_INT_ONE;
         holder = new Holder(address);
-        holder.amount = BigDecimal.zero();
+        holder.amount = BIG_DECIMAL_ZERO;
     }
     holder.amount = holder.amount.plus(amount);
-    if (holder.amount == BigDecimal.zero()) {
+    if (holder.amount == BIG_DECIMAL_ZERO) {
+        delta = BigInt.fromI32(-1);
         store.remove("Holder", address.toHexString());
     } else {
         holder.save();
     }
+    if (delta == BigInt.zero()) {
+        return;
+    }
+    let overview = loadOrNewOverview();
+    overview.holders = overview.holders.plus(delta);
+    overview.save();
 }
