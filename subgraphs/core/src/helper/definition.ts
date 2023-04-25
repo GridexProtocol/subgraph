75ff57a6e7722bd1faa8cf520974714afd35b895
simon-gridex <119500274+simon-gridex@users.noreply.github.com>
2023-04-25 18:06:51 +0800

    add Optimism and Mainnet (#23)

--- subgraphs/core/src/helper/definition.ts
+++ subgraphs/core/src/helper/definition.ts
@@ -63,3 +63,56 @@ export const STABLE_COINS_BASE_GOERLI: string[] = [
 ];
 
 export const MINIMUM_USD_LOCKED_BASE_GOERLI = BigDecimal.fromString("200");
+
+// ethereum
+export const WHITELIST_TOKENS_MAINNET: string[] = [
+    "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48", // USDC
+    "0xdac17f958d2ee523a2206206994597c13d831ec7", // USDT
+    "0x0000000000085d4780b73119b644ae5ecd22b376", // TUSD
+    "0x6b175474e89094c44da98b954eedeac495271d0f", // DAI
+    "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2", // WETH
+    "0x1f9840a85d5af5bf1d1762f925bdaddc4201f984", // UNI
+    "0x2260fac5e5542a773aa44fbcfedf7c193bc2c599", // WBTC
+    "0x514910771af9ca656af840dff83e8264ecf986ca", // LINK
+    "0x5a98fcbea516cf06857215779fd812ca3bef1b32", // LDO
+    "0xc944e90c64b2c07662a292be6244bdf05cda44a7", // GRT
+    "0xd533a949740bb3306d119cc777fa900ba034cd52", // CRV
+    "0xba100000625a3754423978a60c9317c58a424e3d", // BAL
+    "0x4fabb145d64652a948d72533023f6e7a623c7c53", // BUSD
+    "0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9", // AAVE
+    "0x7d1afa7b718fb893db30a3abc0cfc608aacfebb0", // MATIC
+];
+
+export const STABLE_COINS_MAINNET: string[] = [
+    "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48", // USDC
+    "0xdac17f958d2ee523a2206206994597c13d831ec7", // USDT
+    "0x0000000000085d4780b73119b644ae5ecd22b376", // TUSD
+    "0x6b175474e89094c44da98b954eedeac495271d0f", // DAI
+];
+
+export const MINIMUM_USD_LOCKED_MAINNET = BigDecimal.fromString("2000");
+
+// optimism
+export const WHITELIST_TOKENS_OPTIMISM: string[] = [
+    "0x7f5c764cbc14f9669b88837ca1490cca17c31607", // USDC
+    "0x94b008aa00579c1307b0ef2c499ad98a8ce58e58", // USDT
+    "0xda10009cbd5d07dd0cecc66161fc93d7c9000da1", // DAI
+    "0x4200000000000000000000000000000000000006", // WETH
+    "0x6fd9d7ad17242c41f7131d257212c54a0e816691", // UNI
+    "0x68f180fcce6836688e9084f035309e29bf0a2095", // WBTC
+    "0x350a791bfc2c21f9ed5d10980dad2e2638ffa7f6", // LINK
+    "0xfdb794692724153d1488ccdbe0c56c252596735f", // LDO
+    "0x0994206dfe8de6ec6920ff4d779b0d950605fb53", // CRV
+    "0xfe8b128ba8c78aabc59d4c64cee7ff28e9379921", // BAL
+    "0x9c9e5fd8bbc25984b178fdce6117defa39d2db39", // BUSD
+    "0x76fb31fb4af56892a25e32cfc43de717950c9278", // AAVE
+    "0x4200000000000000000000000000000000000042", // OP
+];
+
+export const STABLE_COINS_OPTIMISM: string[] = [
+    "0x7f5c764cbc14f9669b88837ca1490cca17c31607", // USDC
+    "0x94b008aa00579c1307b0ef2c499ad98a8ce58e58", // USDT
+    "0xda10009cbd5d07dd0cecc66161fc93d7c9000da1", // DAI
+];
+
+export const MINIMUM_USD_LOCKED_OPTIMISM = BigDecimal.fromString("2000");
